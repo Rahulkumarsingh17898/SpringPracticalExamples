6f15002a1dd6258200239d98f8790b59f799e6aa
rahulkumarsingh <rahulkumarsingh17898@gmail.com>
2025-06-22 14:48:24 +0530

    Initial Commit with springBoot Template

---
+++ SpringLearningSeries/src/test/java/com/RKSCTO/SpringLearningSeries/SpringLearningSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RKSCTO.SpringLearningSeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
