b3d1a64a34da288d2f5746911a45a0d5ac57113c
rahulkumarsingh <rahulkumarsingh17898@gmail.com>
2025-09-13 20:13:15 +0530

    Kafka producer with Serialization yaml based setting

---
+++ SpringbootkafkaDemo/src/main/java/com/rkstech/controller/EventController.java
@@ -0,0 +1,34 @@
+package com.rkstech.controller;
+
+import com.rkstech.dto.Customer;
+import com.rkstech.service.KafkaMessagePublisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/producer-app")
+public class EventController {
+    @Autowired
+    private KafkaMessagePublisher kafkaMessagePublisher;
+
+    @GetMapping("/publish/{message}")
+    public ResponseEntity<?> publishMessage(@PathVariable String message) {
+        try{
+            for(int i=0; i<=10000;i++){
+                kafkaMessagePublisher.sendMessageToTopic(message+" : "+ i);
+            }
+
+//            kafkaMessagePublisher.sendMessageToTopic(message);
+            return ResponseEntity.ok("message published successfully..");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @PostMapping("/publish")
+    public void sendEvents(@RequestBody Customer customer){
+        kafkaMessagePublisher.sendEventsToTopic(customer);
+    }
+}
---
+++ SpringbootkafkaDemo/src/main/java/com/rkstech/service/KafkaMessagePublisher.java
@@ -0,0 +1,71 @@
+package com.rkstech.service;
+
+import com.rkstech.dto.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+    //to publish a mesage from application we need to use some class to talk to kafka that class is KafkaTemplate
+    @Autowired
+    private KafkaTemplate<Object, Customer> kafkaEvents;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessageToTopic(String message){
+        //whenever we allow sb to create topic on behalf of us it will use the default config and create a topic
+
+
+
+//        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("rkstechie-demo1", message);
+
+        // for create a topic with custom config we need to create the topic manually from terminal using the command
+//        rkstechie-custom-topc -> this i created manually from terminal with three partition
+
+//        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("rkstechie-custom-topc", message);
+// this was custom topic created from terminal there is a way from SB also that we can create a topic with custom config
+//rkstechie-custom-java-topic => from kafkaProducerConfig file
+//
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("rkstechie-custom-java-topic-3", message);
+
+
+        // if here we use future.get the thread will wait for the result and slow down the producer
+//        as kaka is a fast event streaming platform thus we should handle the request asynchronously by using a callback methot
+
+        future.whenComplete((result,ex) -> {
+
+            if (ex == null) {
+                System.out.println("sent message=["+ message +"] with offset"+result.getRecordMetadata().offset()+ "with partition nu:" + result.getRecordMetadata().partition() +"]");
+            }
+            else  {
+                System.out.println("unable to send message =[" + message +"] with offset"+result.getRecordMetadata().offset()+ "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+    public void sendEventsToTopic(Customer customer){
+        try {
+
+
+            CompletableFuture<SendResult<Object, Customer>> future = kafkaEvents.send("rkstechie-serialize-3", customer);
+
+            future.whenComplete((result, ex) -> {
+
+                if (ex == null) {
+                    System.out.println("sent message=[" + customer + "] with offset" + result.getRecordMetadata().offset() + "with partition nu:" + result.getRecordMetadata().partition() + "]");
+                } else {
+                    System.out.println("unable to send message =[" + customer + "] with offset" + result.getRecordMetadata().offset() + "] due to : " + ex.getMessage());
+                }
+            });
+
+        } catch (Exception ex){
+            System.out.println("Error while sending events to topic " + ex.getMessage());
+        }
+
+    }
+}
